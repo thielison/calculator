3e38b4f531050527cf1140e0bfeb0712c7f8ef57
Thielison Alves <67246254+thielison@users.noreply.github.com>
2023-04-07 16:38:05 -0300

    Add calculator functions and user input prompts

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let num1 = prompt("First number:");
+let operator = prompt("Operator:");
+let num2 = prompt("Second number:");
 
+function operate(num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    } else {
+        return "Not a valid operator!";
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(operate(num1, num2, operator));
