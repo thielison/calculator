24e404addb3d656447628298e179d2bce8d185bb
Thielison Alves <67246254+thielison@users.noreply.github.com>
2023-04-13 21:09:22 -0300

    Add calc display styling and code to perform basic arithmetic operations
    This commit adds code for performing basic arithmetic operations in the
    calculator by capturing the operands and the operator selected by the user.
    It also adds styling for the calculator display with a flex layout, font
    sizes, and a background color. The code captures the operands entered by
    the user and performs the operation when the equal button is clicked.
    The calculated result is then displayed in the calculator display.

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-const numberButtons = document.querySelectorAll(".number");
-const calculatorDisplay = document.getElementById("display-value");
+const currentOperation = document.getElementById("current-operation");
+const numberInput = document.getElementById("display-value");
 
-// let num1 = prompt("First number:");
-// let operator = prompt("Operator:");
-// let num2 = prompt("Second number:");
-let displayValue = 0;
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equals");
+
+let operand1 = null;
+let operator = "";
+let operand2 = null;
+// let currentDisplayValue = null;
+let clearOnNextNumber = false;
 
 function operate(num1, num2, operator) {
+    // switch case
     if (operator === "+") {
         return add(num1, num2);
     } else if (operator === "-") {
         return subtract(num1, num2);
-    } else if (operator === "*") {
+    } else if (operator === "x") {
         return multiply(num1, num2);
     } else if (operator === "/") {
         return divide(num1, num2);
@@ -37,10 +43,39 @@ function divide(a, b) {
 }
 
 function populateDisplay(e) {
-    calculatorDisplay.textContent += e.target.textContent;
-    displayValue = calculatorDisplay.textContent;
+    if (!operator) {
+        clearOnNextNumber = true;
+        numberInput.textContent += e.target.textContent;
+        operand1 = Number(numberInput.textContent);
+
+        console.log("OPERAND 1 = " + operand1);
+    } else {
+        if (clearOnNextNumber) {
+            clearOnNextNumber = false;
+            numberInput.textContent = "";
+        }
+
+        numberInput.textContent += e.target.textContent;
+        operand2 = Number(numberInput.textContent);
+
+        console.log("OPERAND 2 = " + operand2);
+    }
+}
+
+function initializeOperation(e) {
+    operator = e.target.id;
+    currentOperation.textContent = `${operand1} ${operator}`;
 }
 
 numberButtons.forEach((button) => {
     button.addEventListener("click", populateDisplay);
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", initializeOperation);
+});
+
+equalsButton.addEventListener("click", () => {
+    currentOperation.textContent = `${operand1} ${operator} ${operand2} =`;
+    numberInput.textContent = operate(operand1, operand2, operator);
 });
